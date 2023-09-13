327c2cc9a1d6e499a623705360656b7d2a167bea
ilmar <ilmar1@protonmail.com>
2023-09-13 12:55:13 +0300

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <cafes-component/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import CafesComponent from "@/components/CafesComponent.vue";
+
+export default {
+  name: 'HomeView',
+  components: {CafesComponent},
+}
+</script>
+
+<style>
+.home {
+  //background-color: black;
+
+}
+
+</style>
